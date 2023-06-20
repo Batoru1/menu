2518aedc62b515bfea86fecfce345a89229e7c67
batoru <tbakanas2@gmail.com>
2023-06-20 20:58:20 +0300

    more styling, js asignement

---
+++ app.js
@@ -0,0 +1,9 @@
+//get only unique categories - HARDEST ONE
+//iterate over categories return buttons
+//make sure to select buttons when they are available
+
+//items
+const
+//load itms
+
+//filter items
